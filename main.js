4339bdf83ac4cb11389af4c4758873be78d2c089
ThomaBarb <thomas.barbero0998@gmail.com>
2024-11-06 23:05:42 +0100

    - Working update of messages feed ?

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 let mainWindow;
+let socket;
+const net = require('net');
+PORT = 65433
+IP = '192.168.1.66'
+const client = new net.Socket();
+
+// Create a single WebSocket connection in the main process
+function createSocketConnection() {
+    client.connect(PORT, IP, () => { 
+        console.log("Connected to server.")
+    }   )
+  
+    // client.on('message', (message) => {
+    //   // Relay messages to renderer when received
+    //   if (mainWindow) {
+    //     mainWindow.webContents.send('socket-message', message);
+    //   }
+    // });
+  
+    client.on('close', () => {
+      console.log('WebSocket closed');
+    });
+}
 
 function createWindow() {
     mainWindow = new BrowserWindow({
@@ -13,23 +36,27 @@ function createWindow() {
 
     mainWindow.loadFile('index.html'); // Load the HTML for your chat interface
 
+    createSocketConnection()
+
     // Requesting data from the server for initialization of the content
-    const net = require('net');
-    PORT = 65433
-    IP = '192.168.1.66'
-    const client = new net.Socket();
-    client.connect(PORT, IP, () => {    
-        client.write('[[-INIT-]]');
-
-    });
-    client.on('error', (error) => {
-        console.error('Connection error:', error); 
-    });
-
-    // Listen for data from the server
+    client.write('[[-INIT-]]');
     client.on('data', (data) => {
-        console.log('Received from server:', data.toString());
-        mainWindow.webContents.send('init-content', data.toString());
+        const parsedMessage = JSON.parse(data);
+        console.log('Received from server:', data.toString('utf-8'));
+        switch (parsedMessage.type.toString('utf-8')) {
+            case 'init':
+                console.log("INIT !!!");
+                mainWindow.webContents.send('init-content', parsedMessage.content);
+                break;
+            // End case init
+
+            case 'answer':
+                console.log("Creating new message");
+                mainWindow.webContents.send('new-message-to-feed', parsedMessage.content);
+                break;
+            // End case answer
+
+        } // End switch
     });
 
 }
@@ -56,4 +83,9 @@ app.on('activate', () => {
 
 ipcMain.on('log-message', (event, message) => {
     console.log('Received from renderer:', message);
+});
+
+ipcMain.on('contact-server', (event, message) => {
+    console.log('Sending to server:', message);
+    client.write(message)
 });
--- renderer.js
+++ renderer.js
@@ -13,7 +13,7 @@ document.getElementById('toggle-button').addEventListener('click', () => {
 
 document.getElementById('ping-server').addEventListener('click', () => {
     const net = require('net');
-    PORT = 65433
+    PORT = 65432
     IP = '192.168.1.66'
     const client = new net.Socket();
     ipcRenderer.send('log-message', 'Trying to connect !'); 
@@ -30,64 +30,107 @@ document.getElementById('ping-server').addEventListener('click', () => {
 
 
 document.getElementById('submit-prompt').addEventListener('click', () => {
-    const net = require('net');
-    PORT = 65433
-    IP = '192.168.1.66'
-    const client = new net.Socket();
-    ipcRenderer.send('log-message', 'Trying to connect !'); 
-    client.connect(PORT, IP, () => {    
-        ipcRenderer.send('log-message', 'Connected !'); 
-        const prompt = document.getElementById("prompt-textarea").value;
-        ipcRenderer.send('log-message', prompt);
-        client.write(prompt);
-    });
-    // Error handling
-    client.on('error', (error) => {
-        ipcRenderer.send('log-message', `Connection error: ${error.message}`);
-        console.error('Connection error:', error);
+    ipcRenderer.send("log-message", "Submitting prompt")
+    const prompt = document.getElementById("prompt-textarea").value;
+    ipcRenderer.send('contact-server', prompt);
+});
+
+// 
+function addMessageContent_empty(message)
+{
+    // let results = [];
+    // results.push({ "id": "system", "message": message });
+    // results.push({ "id": "user", "message": message });
+    // results.push({ "id": "assistant", "message": "" });
+    // return results;
+
+    ////////////////////////////////////////////////////////////////////////////////
+    let results = [];
+    try {
+        // Regular expression to match the segments between start_header_id and eot_id
+        // const regex = /<\|start_header_id\|>(.*?)<\|end_header_id\|>\s*(.*?)<\|eot_id\|>/g;
+        // const regex = /<\|start_header_id\|>(.*?)<\|end_header_id\|>\s*(.*?)\s*<\|eot_id\|>/gs;
+        const regex = /<\|start_header_id\|>(.*?)<\|end_header_id\|>\s*(.*?)\s*<\|eot_id\|>\s*/gs;
+
+        let matches;
+
+        while ((matches = regex.exec(message.toString('utf-8'))) !== null) {
+            const id = matches[1];
+            const message = matches[2];
+            results.push({ id, message });
+        }
+    } catch (error) {
+        ipcRenderer.send("log-message", error.message);
+    }
+    ////////////////////////////////////////////////////////////////////////////////
+    return results
+
+} // End function addMessageContent_empty
+
+// 
+function addMessageContent_allText(message)
+{
+    ipcRenderer.send('log-message', message);
+    ////////////////////////////////////////////////////////////////////////////////
+    let results = [];
+    try {
+        // Regular expression to match the segments between start_header_id and eot_id
+        // const regex = /<\|start_header_id\|>(.*?)<\|end_header_id\|>\s*(.*?)<\|eot_id\|>/g;
+        const regex = /<\|start_header_id\|>(.*?)<\|end_header_id\|>\s*(.*?)\s*<\|eot_id\|>/gs;
+
+        let matches;
+
+        while ((matches = regex.exec(message[1].toString('utf-8'))) !== null) {
+            const id = matches[1];
+            const message = matches[2];
+            results.push({ id, message });
+        }
+    } catch (error) {
+        ipcRenderer.send("log-message", error.message);
+    }
+    ////////////////////////////////////////////////////////////////////////////////
+    return results
+
+} // End function addMessageContent_allText
+
+// 
+function addMessageToFeed(message, new_msg=false)
+{
+    const containerDiv = document.createElement('div');
+    containerDiv.className = 'message';
+    const questionDiv = document.createElement('div');
+    questionDiv.className = 'question';
+    const answerDiv = document.createElement('div');
+    answerDiv.className = 'answer';
+
+    let results = [];
+    if (!new_msg) 
+    {
+        results = addMessageContent_allText(message);
+    }
+    else
+    {
+        results = addMessageContent_empty(message);
+    }
+    ipcRenderer.send('log-message', "results");
+    ipcRenderer.send('log-message', results);
+
+    questionDiv.textContent = results.find(item => item.id === "user").message;
+    answerDiv.textContent = results.find(item => item.id === "assistant").message;
+
+    containerDiv.appendChild(questionDiv);
+    containerDiv.appendChild(answerDiv);  
+    document.getElementById('chatFeed').appendChild(containerDiv);
+
+} // End function addMessageToFeed
+
+ipcRenderer.on('init-content', (event, message) => {    
+    message.forEach(message => {
+        addMessageToFeed(message, false);
     });
 });
 
-
-ipcRenderer.on('init-content', (event, message) => {
-    const jsonData = JSON.parse(message.toString('utf-8'));
-    // ipcRenderer.send('log-message', `Message from server: ${jsonData}`);
-
-    
-    jsonData.forEach(message => {
-        // ipcRenderer.send('log-message', message[1]);
-        const containerDiv = document.createElement('div');
-        containerDiv.className = 'message';
-        const questionDiv = document.createElement('div');
-        questionDiv.className = 'question';
-        const answerDiv = document.createElement('div');
-        answerDiv.className = 'answer';
-
-        ////////////////////////////////////////////////////////////////////////////////
-        const results = [];
-        try {
-            // Regular expression to match the segments between start_header_id and eot_id
-            const regex = /<\|start_header_id\|>(.*?)<\|end_header_id\|>\s*(.*?)<\|eot_id\|>/g;
-
-            let matches;
-
-            while ((matches = regex.exec(message[1])) !== null) {
-                const id = matches[1];
-                const message = matches[2];
-                results.push({ id, message });
-            }
-            // ipcRenderer.send('log-message', results.find(item => item.id === "user"));
-        } catch (error) {
-            ipcRenderer.send("log-message", error.message);
-        }
-        ////////////////////////////////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////////////////////////////////
-
-        questionDiv.textContent = results.find(item => item.id === "user").message;
-        answerDiv.textContent = results.find(item => item.id === "assistant").message;
-
-        containerDiv.appendChild(questionDiv);
-        containerDiv.appendChild(answerDiv);  
-        document.getElementById('chatFeed').appendChild(containerDiv);
-    });
+ipcRenderer.on('new-message-to-feed', (event, message) => {    
+    // addMessageToFeed(message, true);
+    addMessageToFeed(message, true);
 });
